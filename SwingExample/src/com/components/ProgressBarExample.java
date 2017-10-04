4684a5c63099774aa76e33208d512c45a941db38
codetamil <sairam.s2002@gmail.com>
2017-10-04 12:44:17 +0530

    About Components in Swing
    Progress Bars

---
+++ SwingExample/src/com/components/ProgressBarExample.java
@@ -0,0 +1,40 @@
+package com.components;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+
+public class ProgressBarExample extends JFrame{
+	
+	
+	JProgressBar jb;    
+	int i=0,num=0;     
+	
+	
+	ProgressBarExample(){    
+		jb=new JProgressBar(0,2000);    
+		jb.setBounds(40,40,160,30);         
+		jb.setValue(0);    
+		jb.setStringPainted(true);    
+		add(jb);    
+		setSize(250,150);    
+		setLayout(null);    
+		}    
+	
+	public void iterate(){    
+		while(i<=2000){    
+		  jb.setValue(i);    
+		  i=i+100;    
+		  try{Thread.sleep(1500);}catch(Exception e){}    
+		}    
+		}    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 ProgressBarExample m=new ProgressBarExample();    
+		    m.setVisible(true);    
+		    m.iterate(); 
+
+	}
+
+}
